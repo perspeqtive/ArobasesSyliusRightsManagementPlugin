369d7c8496a63d2839f118da8c8396ad4efe87dd
MarkusHolstein <mh@perspeqtive.de>
2026-03-27 11:20:17 +0100

    Ermöglicht das Laden von Entities mit Order Relation
    Da beispielsweise Payment und Shipment nicht direkt mit einem Channel connectd sind, sondern über die Order, muss das TargetEntity über die Order-Relation gefiltert werden

--- src/Access/Doctrine/ChannelFilter.php
+++ src/Access/Doctrine/ChannelFilter.php
@@ -14,14 +14,27 @@ class ChannelFilter extends SQLFilter
 
     public function addFilterConstraint(ClassMetadata $targetEntity, $targetTableAlias): string
     {
-        if (!$targetEntity->reflClass->implementsInterface(ChannelAwareInterface::class)) {
-            return '';
+        if ($targetEntity->reflClass->implementsInterface(ChannelAwareInterface::class)) {
+            return sprintf('%s.channel_id IN (%s)',
+                $targetTableAlias,
+                $this->getAllowedChannels()
+            );
         }
 
-        return sprintf('%s.channel_id IN (%s)',
-            $targetTableAlias,
-            $this->getAllowedChannels()
-        );
+        if ($targetEntity->hasAssociation('order')) {
+            $orderMapping = $targetEntity->getAssociationMapping('order');
+            if (isset($orderMapping['targetEntity']) && is_subclass_of($orderMapping['targetEntity'], ChannelAwareInterface::class)) {
+                $columnName = $orderMapping['joinColumns'][0]['name'] ?? 'order_id';
+
+                return sprintf('%s.%s IN (SELECT id FROM sylius_order WHERE channel_id IN (%s))',
+                    $targetTableAlias,
+                    $columnName,
+                    $this->getAllowedChannels()
+                );
+            }
+        }
+
+        return '';
     }
 
     public function setAllowedChannels(array $channels = []): void {
